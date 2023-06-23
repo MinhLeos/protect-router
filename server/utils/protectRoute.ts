2f1221b70bc5d670213a2df9ee5a9565433a3b5f
Minh Le <minh.le@mgisolutions.com>
2023-06-23 10:26:31 +0700

    add check permission of user

--- server/utils/protectRoute.ts
+++ server/utils/protectRoute.ts
@@ -1,12 +1,24 @@
 import { H3Event } from 'h3';
+import users from '@/assets/data/user.json';
 
 export default async (event: H3Event) => {
     const { headers } = event.node.req;
     console.log('headers.authorization', headers.authorization);
-
     if (!headers.authorization) {
-        console.log('-------');
+        throw createError({
+            statusCode: 401,
+            statusMessage: 'Unauthorized',
+            message: 'Unauthorized'
+        });
+    }
+    //Get Random userID and check Permission
+    const userId = Math.floor(Math.random() * 22) + 1;
+    console.log('userId', userId);
 
+    //Check permission
+    const user = users.find(us => us.id === userId);
+    console.log('user', user);
+    if (!user || !user.isHavePermission) {
         throw createError({
             statusCode: 401,
             statusMessage: 'Unauthorized',
